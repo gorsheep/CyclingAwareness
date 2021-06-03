cb51986deffca6f6227f7024e284c3cb626e3857
Daniel Voloshin <gorsheepe@gmail.com>
2021-06-03 21:56:19 +0300

    Реализован приём изображений по UDP

--- Cycling Awareness/ViewController.swift
+++ Cycling Awareness/ViewController.swift
@@ -6,17 +6,29 @@
 //
 
 import UIKit
+import CocoaAsyncSocket //File -> Swift Packages -> Import CocoaAsyncSocket
 import SceneKit
 import CoreML
 import Vision
 import ImageIO
 
-class ViewController: UIViewController, UINavigationControllerDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, GCDAsyncUdpSocketDelegate {
     
-    //Адрес изображения на локальном сервере
-    //Ноут на винде имеет адрес   172.20.10.2
-    //Мой любимый мак имеет адрес 172.20.10.4
-    var imageURl = "http://172.20.10.4:8080/frame.jpg"
+    //Аутлеты UI элементов
+    @IBOutlet weak var sceneView: SCNView?
+    @IBOutlet weak var newImageView: UIImageView?
+    
+    //Параметры UDP обмена
+    var socket : GCDAsyncUdpSocket?
+    let PORT : UInt16 = 14000           //порт UDP-сокета
+    let IP = "172.20.10.1"              //адрес айфона
+    let bufferSize = 8192               //длина буфера (одного UDP-пакета)
+    let headerSize = 5                  //длина заголовка, в котором хранится ожидаемое количество пакетов
+    var imageData : Data? = "".data(using: .utf8)  //сюда будет записываться изображение (оно передаётся пакетами по bufferSize байт)
+    var packetCounter = 0               //счётчик принятых пакетов
+    var numOfPackets  = 0               //ожидаемое число пакетов
+    var waitingForFirstPacket = true    //флаг того, что мы ждём пакет с заголовком
+    var badFramesCounter = 0            //отладочная переменная
     
     //Период основного цикла, секунд
     let cycleLength = 1
@@ -38,13 +50,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     //Массив машин
     var cars: [SCNNode] = []
 
-    //Аутлеты UI элементов
-    @IBOutlet weak var sceneView: SCNView?
-    @IBOutlet weak var newImageView: UIImageView?
-    
     //Переменные для переключения между картинками
     var img = 0
     var imgIterator = 0
+    
     
     
     //Функция, которая выполняется, когда приложение запускается
@@ -53,6 +62,14 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         
         //Создаём таймер для цикла, в котором будем обрабатывать изображения
         let timer = Timer.scheduledTimer(timeInterval: TimeInterval(cycleLength), target: self, selector: #selector(cycle), userInfo: nil, repeats: true)
+        
+        //Создаём UDP-сокет
+        let socket = GCDAsyncUdpSocket(delegate: self, delegateQueue: DispatchQueue.main)
+        do {
+            try socket.bind(toPort: PORT)
+            try socket.enableBroadcast(true)
+            try socket.beginReceiving()
+        } catch _ as NSError { print("Issue with setting up listener") }
         
         //Устанавливаем режим сглаживания (antialiasing)
         sceneView?.antialiasingMode = SCNAntialiasingMode.multisampling4X
@@ -85,26 +102,13 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         
         //Обращение к i-той машине
         //print("Global Coordinates: ", cars[4].simdWorldPosition as Any)
-        
-        
-        
-        //Выводим в консоль координаты машины
-        //print(car1Node.simdWorldPosition)  //в глобальной СК
-        //print(car1Node.position)           //в локальной СК
-        
-        //Прячем машину
-        //car1Node.isHidden = true
-        //Показываем машину
-        //car1Node.isHidden = false
     }
  
     
     
     lazy var detectionRequest: VNCoreMLRequest = {
         do {
-            //let model = try VNCoreMLModel(for: Road_Sign_Object_Detector_1().model) //модель со знаками
             let model = try VNCoreMLModel(for: CarsDetectorV1_1().model) //модель с машинами
-            
             let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
                 self?.processDetections(for: request, error: error)
             })
@@ -116,39 +120,14 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     }()
     
     
+    
     //Функция, которая исполняется циклически по таймеру
-    @objc func cycle()
-    {
-        /*
-        //Video capture area
-        if (imgIterator == 0) {
-            //sleep(10)
-        }
+    @objc func cycle() {
         
-        imgIterator = imgIterator+1
-        if (imgIterator < 860) {
-            imageURl = "http://172.20.10.4:8080/Video/Frames-1/frame-" + String(imgIterator) + ".jpg"
-        }
-        */
+        //Тело цикла
         
-        
-        //Захватываем изображение по HTTP
-        guard let url = URL(string: imageURl) else {return}
-        
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    
-                    //Вызываем функию обработки полученного изображения
-                    self?.updateDetections(for: image)
-                    DispatchQueue.main.async {
-                        //Вызываем функцию, которая выводит изображение на экран
-                        self?.newImageView?.image = image
-                    }
-                }
-            }
-        }
-    }
+    }//end of function cycle()
+    
     
     
     //Функция, которая исполняется по нажатии на кнопку
@@ -158,30 +137,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         //Запускаем таймер для расчёта времени, которое уходит на обработку изображения
         let begin = clock()
         
-        //Обновляем позицию машины
-        //let newPosition = SCNVector3(x: 0, y: 0, z: cars[4].position.z + 1000/3)
-        //cars[4].position = newPosition
-        
-        //Выводим в консоль координаты машины
-        //print(car5Node.simdWorldPosition)  //в глобальной СК
-        //print(car5Node.position)           //в локальной СК
-        
         //Обновляем переменные для переключения изображения
         imgIterator = imgIterator+1
-        img = imgIterator%4 //[0,3]
         
-        switch img {
-        case 0:
-            imageURl = "http://172.20.10.4:8080/frame.jpg"
-        case 1:
-            imageURl = "http://172.20.10.4:8080/frame.jpg"
-        case 2:
-            imageURl = "http://172.20.10.4:8080/frame.jpg"
-        case 3:
-            imageURl = "http://172.20.10.4:8080/frame.jpg"
-        default:
-            imageURl = "http://172.20.10.4:8080/frame.jpg"
-        }
+        let imageURl = "http://172.20.10.4:8080/Video/Frames-1/frame-" + String(imgIterator) + ".jpg"
         
         //Захватываем изображение по HTTP
         guard let url = URL(string: imageURl) else {return}
@@ -203,7 +162,9 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         //Выводим время, которое ушло на выполнение функции
         let diff = Double(clock() - begin) / Double(CLOCKS_PER_SEC)
         print("Elapsed time: ", diff, " seconds")
-    }
+        
+    }//end of function testPhoto()
+    
     
     
     private func updateDetections(for image: UIImage) {
@@ -219,7 +180,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
                 print("Failed to perform detection.\n\(error.localizedDescription)")
             }
         }
-    }
+        
+    }//end of function updateDetections()
+    
+    
     
     private func processDetections(for request: VNRequest, error: Error?) {
         DispatchQueue.main.async {
@@ -231,7 +195,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
             let detections = results as! [VNRecognizedObjectObservation]
             self.drawDetectionsOnPreview(detections: detections)
         }
-    }
+        
+    }//end of function processDetections()
+    
+    
     
     func drawDetectionsOnPreview(detections: [VNRecognizedObjectObservation]) {
         guard let image = self.newImageView?.image else {
@@ -299,8 +266,62 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         self.newImageView?.image = newImage
-    }
+        
+    }//end of function drawDetectionsOnPreview()
     
+    
+    
+    //Функция приёма пакета
+    func udpSocket(_ sock: GCDAsyncUdpSocket, didReceive data: Data, fromAddress address: Data, withFilterContext filterContext: Any?) {
+        
+        packetCounter += 1
+        //print("packetCounter: ", packetCounter, ", data: ", data.count, "bytes")
+        //labelField?.text = String(packetCounter) + " packets received"
+        
+        //Получили нулевой пакет, содержащий кол-во пакетов, в которых будет изображение
+        if (packetCounter == 1)&&(waitingForFirstPacket == true)&&(data.count == headerSize) {
+            let str = String(decoding: data, as: UTF8.self)
+            numOfPackets = Int(str)!
+            waitingForFirstPacket = false
+            //print(numOfPackets)
+            return;
+        }
+        
+        //Получили последний пакет
+        if (packetCounter == numOfPackets + 1) {
+            imageData!.append(data)
+            packetCounter = 0
+            waitingForFirstPacket = true
+            
+            //Создаём изображение
+            if let image = UIImage(data: imageData!) {
+                //Вызываем функию обработки полученного изображения
+                self.updateDetections(for: image)
+                DispatchQueue.main.async {
+                    //Вызываем функцию, которая выводит изображение на экран
+                    self.newImageView?.image = image
+                }
+            }
+            
+            return;
+        }
+
+        //Получили пакет, который и не первый, и не последний
+        //Если пакет битый (меньше bufferSize байт), то обнуляем буфер и ждём снова первого пакета
+        if (data.count == bufferSize) {
+            imageData!.append(data)
+        }else{
+            badFramesCounter += 1
+            //print("Битый кадр!")
+            //print("badFramesCounter: ", badFramesCounter)
+            //labelField?.text = "badFramesCounter: " + String(badFramesCounter)
+            //Очищаем буфер для изображения
+            imageData = "".data(using: .utf8)
+            packetCounter = 0
+            waitingForFirstPacket = true
+        }
+        
+    }//end of function udpSocket()
     
     
 }
